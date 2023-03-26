ffa0c387c9830d19ab400c7b8303a5633ed0659a
Kgiruc <kgiruc76@gmail.com>
2023-03-26 16:14:45 +0200

    popup styles

--- gnNews/src/components/Popup/PopupNews.jsx
+++ gnNews/src/components/Popup/PopupNews.jsx
@@ -1,12 +1,14 @@
 function PopupNews({ newscontent, newsauthor, newsurl, setOpen }) {
-  return (
-    <div>
-        <button onClick={() => setOpen()}>x</button>
-        <p>{newscontent}</p>
-        <p>{newsauthor}</p>
-        <a href={newsurl}>{newsurl}</a>
-    </div>
-  )
+    return (
+        <div className="popup__background">
+            <div className="popup__container">
+                <button onClick={() => setOpen()}>x</button>
+                <p>{newscontent}</p>
+                <p>{newsauthor}</p>
+                <a href={newsurl}>url</a>
+            </div>
+        </div>
+    )
 }
 
 export default PopupNews
