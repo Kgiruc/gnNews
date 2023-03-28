b9880a33371298d60ad60d8745a8f0c3b00ef84d
Kgiruc <kgiruc76@gmail.com>
2023-03-29 00:27:16 +0200

    headers styles

--- gnNews/src/components/Header.jsx
+++ gnNews/src/components/Header.jsx
@@ -1,16 +1,23 @@
 import { Link } from "react-router-dom"
 import { useDispatch } from "react-redux"
+import { useSelector } from "react-redux"
 import { changeView } from "../app/features/viewSlice"
+import popup from "../assets/icons/pop-up.png"
+import list from "../assets/icons/list.png"
+import tile from "../assets/icons/tile.png"
 
 function Header() {
   const dispath = useDispatch()
+  const view = useSelector((state) => state.view.viewState)
 
   return (
     <header>
       <h1><Link to="/">gnNews</Link></h1>
       <nav>
-        <button onClick={() => dispath(changeView())}>change view</button>
-        <button>popup</button>
+        <button onClick={() => dispath(changeView())}>
+          {view ? <img src={tile} alt="tile" /> : <img src={list} alt="list" />}
+        </button>
+        <button><img src={popup} alt="pop up"/></button>
       </nav>
     </header>
   )
